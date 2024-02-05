ceeb63e6d9aded943df755dcbc472ec2a52c390b
Jory Fahad <158813206+JoryF@users.noreply.github.com>
2024-02-05 22:29:50 +0300

    Add files via upload

--- main (3).dart
+++ main (3).dart
@@ -7,15 +7,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
+//this widget is the root of your application
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'game for you',
       theme: ThemeData(
         // This is the theme of your application.
-        //
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a blue toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
@@ -124,3 +123,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+//end
